e345ccf3d0752cff2d82b2f07aac80b3be267199
rlw92 <r_l_williams@yahoo.com>
2024-04-21 10:39:43 +0100

    Added about info. need to work out how to upload profile pic.

--- haunted-inventory/app/Http/Controllers/ProfileController.php
+++ haunted-inventory/app/Http/Controllers/ProfileController.php
@@ -34,12 +34,17 @@ class ProfileController extends Controller
      * Update the user's profile information.
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
+    { 
+       // dd($request->hasFile('profilePic'));
+        
         $request->user()->fill($request->validated());
+               
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
+        
+        
 
         $request->user()->save();
 
